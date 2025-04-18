e8f4dab251c691650977e24227b62ca86654e60f
SAUMYA MEHTA <113824023+saumyyaa@users.noreply.github.com>
2025-04-18 14:53:18 +0530

    Create auth.js

---
+++ __mocks__/firebase/auth.js
@@ -0,0 +1,4 @@
+export const signInWithEmailAndPassword = jest.fn();
+export const createUserWithEmailAndPassword = jest.fn();
+export const signOut = jest.fn();
+// Add other Firebase auth functions you use
